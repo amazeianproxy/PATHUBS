4949697c08d66a751d3d3f20a27f19efb5645cb2
Ian M. <amazeianproxy@gmail.com>
2025-06-14 22:56:55 +0700

    Forum Card Update

--- src/app/Component/forum_card.tsx
+++ src/app/Component/forum_card.tsx
@@ -5,16 +5,25 @@ type ForumCardProps = {
   forum_id: number;
   title: string;
   content: string;
+  image_url?: string; // Add image_url as an optional prop
 };
 
-export default function ForumCard({ forum_id, title, content }: ForumCardProps) {
+export default function ForumCard({ forum_id, title, content, image_url }: ForumCardProps) {
+
+  const words = content.split(" ");
+  const displayContent = words.length > 200 ? words.slice(0, 200).join(" ") + "..." : content;
+
   return (
     <Link href={`/Pages/ForumPost`}>
-      <div className="post-card cursor-pointer">
-        <img src="https://i.imgur.com/MK3eW3As.png" alt="sprite" className="post-img" />
-        <div className="post-content">
+      <div className="post-card cursor-pointer flex">
+        <img
+          src={image_url ? image_url : "/assets/shelter2.jpg"}
+          alt="sprite"
+          className="w-64 h-40 rounded mr-4"
+        />
+        <div className="bg-white p-4 rounded-xl flex flex-col justify-between">
           <h2 className="text-xl font-bold">{title}</h2>
-          <p>{content}</p>
+          <p>{displayContent}</p>
         </div>
       </div>
     </Link>
