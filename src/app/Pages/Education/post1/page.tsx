80878ac268dbfa0fa05dde82d6e3bc1cbfc27254
Renaldo <renaldornzx@gmail.com>
2025-06-14 23:17:57 +0700

    Update page.tsx

--- src/app/Pages/Education/post1/page.tsx
+++ src/app/Pages/Education/post1/page.tsx
@@ -43,7 +43,7 @@ const InsideEdu = () => {
       {/* Judul */}
       <div className="bg-[#FCD34D] py-2 text-center">
         <h1 className="bg-[#FDF2D5] text-5xl font-bold text-black text-center px-6 py-4 rounded-3xl inline-block mx-auto shadow">
-            How to Train a dragon?
+            Basic Pet Care Tips?
         </h1>
       </div>
 
@@ -52,24 +52,30 @@ const InsideEdu = () => {
         <div className="bg-[#FDF2D5] flex flex-col md:flex-row items-center md:items-start rounded-3xl shadow-xl max-w-6xl w-full p-10 space-y-8 md:space-y-0 md:space-x-10">
           {/* Gambar */}
           <img
-            src="https://upload.wikimedia.org/wikipedia/en/e/e8/Pokemon_Red_English_box_art.png"
+            src="/assets/1.jpeg"
             alt="Post Thumbnail"
-            className="w-80 h-80 rounded-3xl object-cover"
+            className="w-80 h-80 rounded-3xl object-cover self-center md:self-start mt-20"
           />
 
           {/* Konten Deskripsi */}
           <div className="bg-white rounded-3xl p-10 flex-1">
             <h2 className="text-3xl font-bold mb-6 text-center md:text-left text-black">
-              How to Train a dragon?
+              "Basic Pet Care Tips!"
             </h2>
             <p className="text-lg text-gray-800 leading-relaxed text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit.
+              Owning a pet is a joyful experience, but it comes with responsibility. 
+              Here are key points to keep in mind:
+              <br/>
+              1.Choose the Right Pet: Match the pet to your lifestyle. Dogs need more time and space; cats are more independent.
+              <br/>
+              2.Health & Nutrition: Feed them a proper diet, provide clean water, and schedule regular vet visits.
+              <br/>
+              3.Training & Socialization: Teach good behavior and help them get comfortable with people and other animals.
+              <br/>
+              4.Exercise & Enrichment: Keep pets active and mentally engaged with toys, walks, or playtime.
+              <br/>
+              5.Long-Term Commitment: Pets depend on you for their entire lives—be ready for the journey.
+              <br/>
             </p>
           </div>
         </div>
